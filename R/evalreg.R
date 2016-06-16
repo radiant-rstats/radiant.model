f657aca238d2b024bfcbd1e3fc33c7783af22b80
vnijs <radiant@rady.ucsd.edu>
2016-06-16 13:54:02 -0700

    Evaluation for regression models

---
+++ R/evalreg.R
@@ -0,0 +1,106 @@
+#' Model evalreg
+#'
+#' @details See \url{http://vnijs.github.io/radiant/analytics/evalreg.html} for an example in Radiant
+#'
+#' @param dataset Dataset name (string). This can be a dataframe in the global environment or an element in an r_data list from Radiant
+#' @param pred Predictions or predictors
+#' @param rvar Response variable
+#' @param train Use data from training ("Training"), validation ("Validation"), both ("Both"), or all data ("All") to evaluate model evalreg
+#' @param data_filter Expression entered in, e.g., Data > View to filter the dataset in Radiant. The expression should be a string (e.g., "price > 10000")
+#'
+#' @return A list of results
+#'
+#' @seealso \code{\link{summary.evalreg}} to summarize results
+#' @seealso \code{\link{plot.evalreg}} to plot results
+#'
+#' @export
+evalreg <- function(dataset, pred, rvar,
+                    train = "",
+                    data_filter = "") {
+
+	dat_list <- list()
+	vars <- c(pred, rvar)
+	if (train == "Both") {
+		dat_list[["Training"]] <- getdata(dataset, vars, filt = data_filter)
+		dat_list[["Validation"]] <- getdata(dataset, vars, filt = paste0("!(",data_filter,")"))
+	} else if (train == "Training") {
+		dat_list[["Training"]] <- getdata(dataset, vars, filt = data_filter)
+	} else if (train == "Validation") {
+		dat_list[["Validation"]] <- getdata(dataset, vars, filt = paste0("!(",data_filter,")"))
+	} else {
+		dat_list[["All"]] <- getdata(dataset, vars, filt = "")
+	}
+
+	if (!is_string(dataset)) dataset <- "-----"
+
+
+	pdat <- list()
+	for (i in names(dat_list)) {
+		dat <- dat_list[[i]]
+	  rv <- dat[[rvar]]
+
+	  ## see http://stackoverflow.com/a/35617817/1974918 about extracting a row
+	  ## from a tbl_df
+	  pdat[[i]] <-
+		  data.frame(
+		    Type = rep(i, length(pred)),
+		    Predictor = pred,
+		    Rsq = cor(rv, dat[pred])^2 %>% .[1,],
+		    RSME = summarise_each_(dat, funs(mean((rv - .)^2, na.rm = TRUE) %>% sqrt), vars = pred) %>% unlist,
+		    MAE = summarise_each_(dat, funs(mean(abs(rv - .), na.rm = TRUE)), vars = pred) %>% unlist
+	    )
+  }
+
+	dat <- bind_rows(pdat) %>% as.data.frame
+	rm(pdat, dat_list)
+
+	environment() %>% as.list %>% set_class(c("evalreg",class(.)))
+}
+
+#' Summary method for the evalreg function
+#'
+#' @details See \url{http://vnijs.github.io/radiant/analytics/evalreg.html} for an example in Radiant
+#'
+#' @param object Return value from \code{\link{evalreg}}
+#' @param ... further arguments passed to or from other methods
+#'
+#' @seealso \code{\link{evalreg}} to summarize results
+#' @seealso \code{\link{plot.evalreg}} to plot results
+#'
+#' @export
+summary.evalreg <- function(object, ...) {
+
+  if (is.character(object)) return(object)
+	cat("Evaluate predictions for regression models\n")
+	cat("Data        :", object$dataset, "\n")
+	if (object$data_filter %>% gsub("\\s","",.) != "")
+		cat("Filter      :", gsub("\\n","", object$data_filter), "\n")
+	cat("Results for :", object$train, "\n")
+	cat("Perdictors  :", paste0(object$pred, collapse=", "), "\n")
+	cat("Response    :", object$rvar, "\n\n")
+	print(dfprint(object$dat), row.names = FALSE)
+}
+
+#' Plot method for the evalreg function
+#'
+#' @details See \url{http://vnijs.github.io/radiant/analytics/evalreg.html} for an example in Radiant
+#'
+#' @param x Return value from \code{\link{evalreg}}
+#' @param shiny Did the function call originate inside a shiny app
+#' @param ... further arguments passed to or from other methods
+#'
+#' @seealso \code{\link{evalreg}} to generate results
+#' @seealso \code{\link{summary.evalreg}} to summarize results
+#'
+#' @export
+plot.evalreg <- function(x, shiny = FALSE, ...) {
+
+	object <- x; rm(x)
+  if (is.character(object) || is.null(object)) return(invisible())
+
+	gather_(object$dat, "Metric", "Value", c("Rsq","RSME","MAE"), factor_key = TRUE) %>%
+		mutate(Predictor = factor(Predictor, levels = unique(Predictor))) %>%
+		visualize(xvar = "Predictor", yvar = "Value", type = "bar",
+		          facet_row = "Metric", fill = "Type", axes = "scale_y", custom = TRUE) +
+		ylab("") + xlab("Predictor")
+}
---
+++ inst/app/tools/analysis/evalreg_ui.R
@@ -0,0 +1,129 @@
+ereg_train <- list("All" = "All", "Training" = "Training", "Validation" = "Validation", "Both" = "Both")
+
+## list of function arguments
+ereg_args <- as.list(formals(evalreg))
+
+ereg_inputs <- reactive({
+  ## loop needed because reactive values don't allow single bracket indexing
+  ereg_args$data_filter <- if (input$show_filter) input$data_filter else ""
+  ereg_args$dataset <- input$dataset
+  for (i in r_drop(names(ereg_args)))
+    ereg_args[[i]] <- input[[paste0("ereg_",i)]]
+  ereg_args
+})
+
+###############################################################
+# Evaluate model evalreg
+###############################################################
+output$ui_ereg_rvar <- renderUI({
+  isNum <- .getclass() %in% c("integer","numeric")
+  vars <- varnames()[isNum]
+  selectInput(inputId = "ereg_rvar", label = "Response variable:", choices = vars,
+    selected = state_single("ereg_rvar", vars), multiple = FALSE)
+})
+
+output$ui_ereg_pred <- renderUI({
+  isNum <- .getclass() %in% c("integer","numeric")
+  vars <- varnames()[isNum]
+
+  req(available(input$ereg_rvar))
+  ## don't use setdiff, removes names
+  if (length(vars) > 0 && input$ereg_rvar %in% vars)
+    vars <- vars[-which(vars == input$ereg_rvar)]
+
+  selectInput(inputId = "ereg_pred", label = "Predictor:", choices = vars,
+    selected = state_multiple("ereg_pred", vars),
+    multiple = TRUE, size = min(4, length(vars)), selectize = FALSE)
+})
+
+output$ui_ereg_train <- renderUI({
+  if (is.null(input$show_filter) || input$show_filter == "FALSE" ||
+      is_empty(input$data_filter)) {
+    ereg_train <- ereg_train[1]
+    r_state$ereg_train <<- ereg_train
+  }
+
+  radioButtons("ereg_train", label = "Show results for:", ereg_train,
+    selected = state_init("ereg_train", "All"),
+    inline = TRUE)
+})
+
+output$ui_evalreg <- renderUI({
+  req(input$dataset)
+  tagList(
+    wellPanel(
+      actionButton("ereg_run", "Evaluate", width = "100%")
+    ),
+  	wellPanel(
+	    uiOutput("ui_ereg_rvar"),
+      uiOutput("ui_ereg_pred"),
+      uiOutput("ui_ereg_train")
+  	),
+  	help_and_report(modal_title = "Model evalreg",
+  	                fun_name = "evalreg",
+  	                help_file = inclMD(file.path(getOption("radiant.path.model"),"app/tools/help/evalreg.md")))
+	)
+})
+
+ereg_plot_width <- function() 650
+ereg_plot_height <- function() 650
+
+## output is called from the main radiant ui.R
+output$evalreg <- renderUI({
+	register_print_output("summary_evalreg", ".summary_evalreg")
+	register_plot_output("plot_evalreg", ".plot_evalreg",
+                       	width_fun = "ereg_plot_width",
+                       	height_fun = "ereg_plot_height")
+
+	## one output with components stacked
+	ereg_output_panels <- tagList(
+      downloadLink("dl_ereg_tab", "", class = "fa fa-download alignright"), br(),
+      verbatimTextOutput("summary_evalreg"),
+      plot_downloader("evalreg", height = ereg_plot_height()),
+      plotOutput("plot_evalreg", height = "100%")
+  )
+
+	stat_tab_panel(menu = "Model > Evaluate",
+	              tool = "Evaluate Regression",
+	              tool_ui = "ui_evalreg",
+	             	output_panels = ereg_output_panels)
+})
+
+.evalreg <- eventReactive(input$ereg_run, {
+  # req(input$ereg_pause == FALSE, cancelOutput = TRUE)
+	do.call(evalreg, ereg_inputs())
+})
+
+.summary_evalreg <- reactive({
+  if (not_available(input$ereg_rvar) || not_available(input$ereg_pred))
+    return("This analysis requires a numeric response variable and one or more\nnumeric predictors. If these variable types are not available please\nselect another dataset.\n\n" %>% suggest_data("diamonds"))
+  if (not_pressed(input$ereg_run)) return("** Press the Evaluate button to evaluate models **")
+  summary(.evalreg())
+})
+
+.plot_evalreg <- reactive({
+  if (not_pressed(input$ereg_run)) return(invisible())
+  if (not_available(input$ereg_rvar) || not_available(input$ereg_pred)) return(" ")
+  req(input$ereg_train)
+  plot(.evalreg(), shiny = TRUE)
+})
+
+observeEvent(input$evalreg_report, {
+  outputs <- c("summary","plot")
+  update_report(inp_main = clean_args(ereg_inputs(), ereg_args),
+                fun_name = "evalreg",
+                inp_out = list("",""),
+                outputs = outputs,
+                figs = TRUE,
+                fig.width = ereg_plot_width(),
+                fig.height = ereg_plot_height())
+})
+
+output$dl_ereg_tab <- downloadHandler(
+  filename = function() { "evalreg.csv" },
+  content = function(file) {
+    do.call(summary, c(list(object = .evalreg()), ereg_inputs(),
+            list(prn = FALSE))) %>%
+      write.csv(., file = file, row.names = FALSE)
+  }
+)
