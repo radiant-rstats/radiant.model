4373140ce713c5c255e1e31bfa75b35d750d2926
vnijs <radiant@rady.ucsd.edu>
2016-09-17 12:36:21 -0700

    update

---
+++ NEWS.md
@@ -0,0 +1,5 @@
+# CHANGES IN radiant.model VERSION 0.6.1 (unreleased)
+
+## NEW FEATURES
+
+## BUG FIXES
---
+++ build/remove_screenshots.R
@@ -0,0 +1,18 @@
+## based on https://gist.github.com/mages/1544009
+cdir <- setwd("~/gh/radiant.model/inst/app/tools/help")
+
+## remove all local png files
+list.files("./figures/", pattern = "*.png")
+unlink("figures/*.png")
+check <- list.files("./figures/", pattern = "*.png")
+stopifnot(length(check) == 0)
+cat("--", file="figures/place_holder.txt")
+
+fn <- list.files(pattern="\\.(md|Rmd)$")
+for (f in fn) {
+  org <- readLines(f)
+  changed <- gsub("figures_model/", "https://radiant-rstats.github.io/docs/model/figures_model/", org)
+  cat(changed, file=f, sep="\n")
+}
+
+setwd(cdir)
