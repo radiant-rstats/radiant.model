b17d85e36cf9a8761abb5eea56f107f2dd3909d7
vnijs <radiant@rady.ucsd.edu>
2016-11-16 11:42:15 -0800

    add arguments

--- inst/app/radiant.R
+++ inst/app/radiant.R
@@ -1,11 +1,12 @@
-predict_plot_controls <- function(type) {
+predict_plot_controls <- function(type, vars_color = NULL, init_color = "none") {
   vars <- input[[paste0(type,"_evar")]]
   xvar <- paste0(type, "_xvar")
   frow <- paste0(type, "_facet_row")
   fcol <- paste0(type, "_facet_col")
   col <- paste0(type, "_color")
   vars_facet <- c("None" = ".", vars)
-  vars_color <- c("None" = "none", vars)
+  if (is.null(vars_color))
+    vars_color <- c("None" = "none", vars)
 
   tagList(
     selectizeInput(xvar, "X-variable:", choices = vars,
@@ -18,7 +19,7 @@ predict_plot_controls <- function(type) {
       selected = state_single(fcol, vars_facet, "."),
       multiple = FALSE),
     selectizeInput(col, "Color:", vars_color,
-      selected = state_single(col, vars_color, "none"),
+      selected = state_single(col, vars_color, init_color),
       multiple = FALSE)
   )
 }
