b2ccf216d17192ba68d88556f8df06d546aa2216
vnijs <radiant@rady.ucsd.edu>
2018-01-08 13:36:31 -0800

    cleanup

--- inst/app/global.R
+++ inst/app/global.R
@@ -14,6 +14,3 @@ addResourcePath("www_model", file.path(getOption("radiant.path.model"), "app/www
 ## loading urls and ui
 source("init.R", encoding = getOption("radiant.encoding"), local = TRUE)
 options(radiant.url.patterns = make_url_patterns())
-
-## print options
-# options(max.print = max(getOption("max.print"), 5000))
