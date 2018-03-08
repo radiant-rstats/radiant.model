8e0cd561968c58bdde7dc48f8ffabd6667f09575
vnijs <radiant@rady.ucsd.edu>
2018-03-08 10:34:08 -0800

    fix keyboard shortcuts

--- inst/app/www/js/store.js
+++ inst/app/www/js/store.js
@@ -1,10 +1,24 @@
 $(document).keydown(function(event) {
   // focusing in text (area) inputs
-  if ($("#ff_store_name").is(":focus") && event.keyCode == 13) {
-    $("#ff_store").click();
-  } else if ($("#km_store_name").is(":focus") && event.keyCode == 13) {
-    $("#km_store").click();
-  } else if ($("#ca_store_pred_name").is(":focus") && event.keyCode == 13) {
-    $("#ca_store_pred").click();
+  if ($("#reg_store_res_name").is(":focus") && event.keyCode == 13) {
+    $("#reg_store_res").click();
+  } else if ($("#reg_store_pred_name").is(":focus") && event.keyCode == 13) {
+    $("#reg_store_pred").click();
+  }else if ($("#logit_store_res_name").is(":focus") && event.keyCode == 13) {
+    $("#logit_store_res").click();
+  } else if ($("#logit_store_pred_name").is(":focus") && event.keyCode == 13) {
+    $("#logit_store_pred").click();
+  } else if ($("#nb_store_pred_name").is(":focus") && event.keyCode == 13) {
+    $("#nb_store_pred").click();
+  } else if ($("#nn_store_res_name").is(":focus") && event.keyCode == 13) {
+    $("#nn_store_res").click();
+  } else if ($("#nn_store_pred_name").is(":focus") && event.keyCode == 13) {
+    $("#nn_store_pred").click();
+  } else if ($("#crtree_store_res_name").is(":focus") && event.keyCode == 13) {
+    $("#crtree_store_res").click();
+  } else if ($("#crtree_store_pred_name").is(":focus") && event.keyCode == 13) {
+    $("#crtree_store_pred").click();
+  } else if ($("#crs_store_pred_name").is(":focus") && event.keyCode == 13) {
+    $("#crs_store_pred").click();
   }
 });
